d732c4df5f6d21165e6b2371641cfba3af134ddd
Ahmed Hamouda <ahm3dhamouda@gmail.com>
2025-04-04 22:51:37 +0200

    prevent routing in case of no auth token

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,16 @@
 import { APP_CONSTANTS } from '$lib';
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
 // Create a hook to handle authentication
 const authHook: Handle = async ({ event, resolve }) => {
     // Get the auth token from cookies
     const authToken = event.cookies.get(APP_CONSTANTS.COOKIES.AUTH.COOKIE_NAME);
+
+    if(event.url.pathname !== '/login' && !authToken) {
+        throw redirect(303, '/login');
+    }
+
     event.locals.authToken = authToken ? JSON.parse(authToken) : null;
 
     return resolve(event);
